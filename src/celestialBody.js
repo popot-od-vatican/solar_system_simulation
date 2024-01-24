9421949825260d2317776fd5ae647456e878a8c5
popot-od-vatican <77698066+popot-od-vatican@users.noreply.github.com>
2024-01-24 12:25:30 +0100

    Add files via upload

---
+++ src/celestialBody.js
@@ -0,0 +1,503 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+
+const modelLoader = new GLTFLoader();
+
+export function distance(x, y, a, b)
+{
+    return Math.sqrt(Math.pow(x-a, 2) + Math.pow(y-b, 2));
+}
+
+export async function loadModelFromFile(src)
+{
+    const p = new Promise((resolve, reject) => {
+        modelLoader.load(src, (object) => {
+            resolve(object);
+        })
+    });
+
+    const obj = await p.then((object) => {return object;});
+    
+    return obj;
+}
+
+export default class celestialBody
+{
+    constructor(name, geometry = null, texture = null, semiMajorAxis = 0, semiMinorAxis = 0, period = 0)
+    {
+        this.name = name;
+        this.semiMajorAxis = semiMajorAxis;
+        this.semiMinorAxis = semiMinorAxis;
+
+        this.isRotating = false;
+        this.position = [0, 0, 0];
+        this.rotationXSpeed = 0;
+        this.rotationYSpeed = 0;
+        this.rotationZSpeed = 0;
+        this.timeElapsed = 0;
+        this.orbitLine = null;
+        this.orbitPointsCount = 512;
+        this.trace = null;
+        this.tracePointsCount = 60;
+        this.model = null;
+        this.fixed = false;
+        this.hidden = false;
+        this.hiddenLabel = false;
+
+        this.objectSystem = new THREE.Object3D();
+        this.objectSystem.name = `${this.name}-Main`;
+
+        this.shortDescription = '';
+        this.longDescription = {};
+
+        if(geometry == null)
+        {
+            geometry = new THREE.SphereGeometry(3.5, 30, 30);
+        }
+
+        if(texture == null)
+        {
+            this.objectMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
+        }
+        else
+        {
+            this.objectMaterial = texture;
+        }
+
+        this.axesHelper = new THREE.AxesHelper(40);
+        this.axesHelper.setColors('red', 'green', 'blue');
+        this.axesHelper.name = `${this.name}-AxesHelper`;
+
+        this.objectDiv = document.createElement('div');
+        this.objectDiv.textContent = this.name;
+
+        this.objectLabel = new CSS2DObject(this.objectDiv);
+
+        this.object = new THREE.Mesh(geometry, this.objectMaterial);
+        this.object.name = this.name;
+        this.objectSystem.name = `${this.name}-System`;
+
+        this.object.add(this.axesHelper);
+        
+        this.objectSystem.add(this.objectLabel);
+        this.objectSystem.add(this.object);
+
+        this.setPosition(this.semiMajorAxis, 0, 0);
+        this.setLabelColor('red');
+        this.setPeriod(period);
+        this.setLabelPosition(0, 3.5 * 2, 0);
+        this.hideAxesHelper();
+        this.makeTrace();
+        this.makeOrbit(0, 0, 0);
+    }
+
+    makeOrbit(centerx, centery, centerz, semiMajorAxis = this.semiMajorAxis, semiMinorAxis = this.semiMinorAxis, pointsCount = this.orbitPointsCount, material = null)
+    {
+        this.semiMajorAxis = semiMajorAxis;
+        this.semiMinorAxis = semiMinorAxis;
+
+        const points = [];
+
+        for(let i = 0; i <= pointsCount; ++i)
+        {
+            const theta = (i / pointsCount) * Math.PI * 2;
+            const x = semiMajorAxis * Math.cos(theta) + centerx;
+            const z = semiMinorAxis * Math.sin(theta) + centerz;
+            points.push(x, centery, z);
+        }
+
+        const ellipseGeometry = new THREE.BufferGeometry();
+        ellipseGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(points), 3));
+        
+        if(material == null)
+        {
+            material = new THREE.LineBasicMaterial({color: 0xffffff});
+        }
+
+        this.orbitLine = new THREE.Line(ellipseGeometry, material);
+    }
+
+    makeTrace()
+    {
+        const points = [];
+
+        for(let i = 0; i <= this.tracePointsCount; ++i)
+        {
+            points.push(this.position[0], this.position[1], this.position[2]);
+        }
+
+        const lineGeo = new THREE.BufferGeometry();
+        lineGeo.setAttribute('position', new THREE.BufferAttribute(new Float32Array(points), 3));
+        const lineMat = new THREE.LineBasicMaterial({color: 0xffffff});
+
+        this.trace = new THREE.Line(lineGeo, lineMat);
+    }
+
+    addTraceToScene(scene)
+    {
+        if(this.trace != null)
+        {
+            scene.add(this.trace);
+        }
+    }
+
+    addOrbitToScene(scene)
+    {
+        if(this.orbitLine != null)
+        {
+            scene.add(this.orbitLine);
+        }
+    }
+
+    setPosition(x, y, z)
+    {
+        this.position = [x, y, z];
+        this.objectSystem.position.set(this.position[0], this.position[1], this.position[2]);
+    }
+
+    getPosition()
+    {
+        return this.position;
+    }
+
+    hideAllOrbits()
+    {
+        this.hideOrbit();
+        this.hideOrbitTrace();
+    }
+
+    show()
+    {
+        this.objectSystem.visible = true;
+        this.showLabel();
+        this.hiddenLabel = false;
+        this.hidden = false;
+    }
+
+    hide()
+    {
+        this.objectSystem.visible = false;
+        this.hideLabel();
+        this.hideOrbit();
+        this.hideOrbitTrace();
+        this.hiddenLabel = true;
+        this.hidden = true;
+    }
+
+    isVisible()
+    {
+        return this.object.visible;
+    }
+
+    showLabel()
+    {
+        this.objectLabel.visible = true;
+        this.hiddenLabel = false;
+    }
+
+    hideLabel()
+    {
+        this.objectLabel.visible = false;
+        this.hiddenLabel = true;
+    }
+
+    setLabelColor(newColor)
+    {
+        this.objectDiv.style.color = newColor;
+    }
+
+    setLabelFontSize(newSize)
+    {
+        this.objectDiv.style.fontSize = `${newSize}px`;
+    }
+
+    setLabelClass(newClass)
+    {
+        this.objectDiv.className = newClass;
+    }
+
+    setLabelPosition(x, y, z)
+    {
+        this.objectLabel.position.set(x, y, z);
+    }
+
+    setLabelCenter(newCenter)
+    {
+        this.objectLabel.center.set(newCenter[0], newCenter[1]);
+    }
+
+    async loadModel(src, scene)
+    {
+        this.model = await loadModelFromFile(src);
+        this.addModelToScene(scene);
+    }
+
+    addModelToScene(scene)
+    {
+        this.objectSystem.remove(this.object);
+
+        this.object = this.model.scene;
+
+        this.object.add(this.axesHelper);
+
+        this.objectSystem.add(this.object);
+    }
+
+    translateX(units)
+    {
+        this.objectSystem.translateX(units);
+    }
+
+    translateY(units)
+    {
+        this.objectSystem.translateY(units);
+    }
+
+    translateZ(units)
+    {
+        this.objectSystem.translateZ(units);
+    }
+
+    rotate(newRotation)
+    {
+        this.object.rotation.set(newRotation[0], newRotation[1], newRotation[2]);
+    }
+
+    rotateBy(x, y, z)
+    {
+        this.object.rotateX(x);
+        this.object.rotateY(y);
+        this.object.rotateZ(z);
+    }
+
+    setShouldRotate(shouldRotate)
+    {
+        this.isRotating = shouldRotate;
+    }
+
+    setRotationXSpeed(newSpeed)
+    {
+        this.rotationXSpeed = newSpeed;
+    }
+
+    setRotationYSpeed(newSpeed)
+    {
+        this.rotationYSpeed = newSpeed;
+    }
+
+    setRotationZSpeed(newSpeed)
+    {
+        this.rotationZSpeed = newSpeed;
+    }
+
+    lookAt(positionToLookAt)
+    {
+        this.object.lookAt(positionToLookAt);
+    }
+
+    setOrbitColor(newColor)
+    {
+        if(this.orbitLine != null)
+        {
+            const newMat = new THREE.LineBasicMaterial({color: newColor});
+            this.orbitLine.material = newMat;
+        }
+    }
+
+    setOrbitTraceColor(newColor)
+    {
+        if(this.trace != null)
+        {
+            const newMat = new THREE.LineBasicMaterial({color: newColor});
+            this.trace.material = newMat;
+        }
+    }
+
+    showOrbit()
+    {
+        if(this.orbitLine != null)
+        {
+            this.orbitLine.visible = true;
+        }
+    }
+
+    showOrbitTrace()
+    {
+        if(this.trace != null)
+        {
+            this.trace.visible = true;
+        }
+    }
+
+    hideOrbit()
+    {
+        if(this.orbitLine != null)
+        {
+            this.orbitLine.visible = false;
+        }
+    }
+
+    hideOrbitTrace()
+    {
+        if(this.trace != null)
+        {
+            this.trace.visible = false;
+        }
+    }
+
+    addToScene(scene)
+    {
+        scene.add(this.objectSystem);
+    }
+
+    hideAxesHelper()
+    {
+        this.axesHelper.visible = false;
+    }
+
+    showAxesHelper()
+    {
+        this.axesHelper.visible = true;
+    }
+
+    setMajorAxis(newSemiMajorAxis)
+    {
+        this.semiMajorAxis = newSemiMajorAxis;
+    }
+
+    setMinorAxis(newSemiMinorAxis)
+    {
+        this.semiMinorAxis = newSemiMinorAxis;
+    }
+
+    setPeriod(newPeriod)
+    {
+        if(newPeriod == 0)
+            newPeriod = 1;
+        this.period = newPeriod;
+        this.speed = ((Math.PI * 2) / this.period)/24/60/60;
+    }
+
+    setOrbitPointsCount(newOrbitPointsCount)
+    {
+        this.orbitPointsCount = newOrbitPointsCount;
+    }
+
+    setTracePointsCount(newTracePointsCount)
+    {
+        this.tracePointsCount = newTracePointsCount;
+    }
+
+    getOrbitPointsCount()
+    {
+        return this.orbitPointsCount;
+    }
+
+    freeze()
+    {
+        this.fixed = true;
+    }
+
+    unfreeze()
+    {
+        this.fixed = false;
+    }
+
+    setShortDescription(text)
+    {
+        this.shortDescription = text;
+    }
+
+    setLongDescription(text)
+    {
+        this.longDescription = text;
+    }
+
+    scale(x, y, z)
+    {
+        this.objectSystem.scale.set(x, y, z);
+    }
+
+    updateLabelVisibility(scene, camera, raycaster)
+    {
+        if(!this.hiddenLabel)
+        {
+            this.objectSystem.getWorldPosition(raycaster.ray.origin);
+            const rd = camera.position.clone().sub(raycaster.ray.origin).normalize();
+            raycaster.ray.direction.set(rd.x, rd.y, rd.z);
+
+            const hits = raycaster.intersectObjects(scene.children, true);
+            let count = hits.length;
+
+
+            for(let j = 0; j < hits.length; ++j) {
+                if(hits[j] != null && hits[j].hasOwnProperty('object')) {
+                    if(hits[j].object.name.includes('Clouds') || hits[j].object.name.includes('Helper') || hits[j].object.type == 'Line') {
+                        --count;
+                    }
+                }
+            }
+
+            if(count >= 1) {
+                this.hideLabel();
+                this.hiddenLabel = false;
+            }
+            else {
+                this.showLabel();
+            }
+        }
+    }
+
+    updateLabel(camera, maxDistance = 10000.0, minDistance = 15.0)
+    {
+        const vec3 = new THREE.Vector3();
+        this.objectSystem.getWorldPosition(vec3);
+        const distance = camera.position.distanceTo(vec3);
+        if(distance >= maxDistance || distance <= minDistance) {
+            this.objectLabel.element.style.opacity = "0";
+        }
+        else {
+            this.objectLabel.element.style.opacity = "1";
+        }
+    }
+
+    update(delta, simulationSpeed)
+    {
+        if(!this.fixed)
+        {
+            this.timeElapsed = this.timeElapsed + (delta * simulationSpeed);
+            
+            if(this.isRotating)
+            {
+                this.object.rotateX(this.rotationXSpeed * delta * simulationSpeed);
+                this.object.rotateY(this.rotationYSpeed * delta * simulationSpeed);
+                this.object.rotateZ(this.rotationZSpeed * delta * simulationSpeed);
+            }
+
+            const x = this.semiMajorAxis * Math.cos(-(this.timeElapsed*this.speed));
+            const z = this.semiMinorAxis * Math.sin(-(this.timeElapsed*this.speed));
+
+            if(this.trace != null)
+            {
+                const traceVertices = this.trace.geometry.attributes.position.array;
+
+                if(distance(x, z, traceVertices[traceVertices.length-3], traceVertices[traceVertices.length-1]) >= 0.1)
+                {
+                    traceVertices[traceVertices.length-3] = x;
+                    traceVertices[traceVertices.length-1] = z;
+
+                    for(let i = 0; i < (traceVertices.length / 3)-1; ++i)
+                    {
+                        traceVertices[i*3] = traceVertices[(i+1)*3];
+                        traceVertices[i * 3 + 2] = traceVertices[(i+1)*3 + 2];
+                    }
+                }
+
+                this.trace.geometry.attributes.position.needsUpdate = true;
+            }
+
+            this.setPosition(x, this.position[1], z);
+        }
+    }
+}
---
+++ src/descriptions.js
@@ -0,0 +1,94 @@
+
+const descriptions  = {
+    mercury: {
+        short: 'The first planet in our solar system',
+        long: {
+            title: "Mercury",
+            paragraph: [
+                "Mercury's surface temperatures are both extremely hot and cold. Because the planet is so close to the Sun,\
+                day temperatures can reach highs of 800°F (430°C).\
+                Without an atmosphere to retain that heat at night, temperatures can dip as low as -290°F (-180°C).",
+                "Despite its proximity to the Sun, Mercury is not the hottest planet in our solar system – that title belongs to nearby Venus, thanks to its dense atmosphere.\
+                But Mercury is the fastest planet, zipping around the Sun every 88 Earth days."
+            ],
+        },
+    },
+    venus: {
+        short: 'The second planet in our solar system',
+        long: {
+            title: "Twin Sister",
+            paragraph: [
+                "Venus is the second planet from the Sun, and our closest planetary neighbor.\
+                It's the hottest planet in our solar system, and is sometimes called Earth's twin.",
+                "Similar in structure and size to Earth, Venus spins slowly in the opposite direction from most planets.\
+                Its thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system with surface temperatures hot enough to melt lead.\
+                Glimpses below the clouds reveal volcanoes and deformed mountains."
+            ]
+        },
+    },
+    earth: {
+        short: 'The third planet in our solar system',
+        long: {
+            title: "Our home - Earth",
+            paragraph: [
+                "Earth—our home planet—is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things.\
+                It's also the only planet in our solar system with liquid water on the surface.",
+                "Earth is only the fifth largest planet in the solar system.\
+                Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal."
+            ]
+        },
+    },
+    mars: {
+        short: 'The fourth planet in our solar system',
+        long: {
+            title: "Red Planet",
+            paragraph: [
+                "Mars is no place for the faint-hearted. It’s dry, rocky, and bitter cold. The fourth planet from the Sun, Mars is one of Earth's two closest planetary neighbors (Venus is the other).\
+                Mars is one of the easiest planets to spot in the night sky – it looks like a bright red point of light.",
+                "Despite being inhospitable to humans, robotic explorers – like NASA's Perseverance rover – are serving as pathfinders to eventually get humans to the surface of the Red Planet."
+            ]
+        },
+    },
+    jupiter: {
+        short: 'The fifth planet in our solar system',
+        long: {
+            title: "Giant Among Giants",
+            paragraph: [
+                "Jupiter is the fifth planet from our Sun and is, by far, the largest planet in the solar system – more than twice as massive as all the other planets combined.",
+                "Jupiter's stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. Jupiter’s iconic Great Red Spot is a giant storm bigger than Earth that has raged for hundreds of years."
+            ]
+        },
+    },
+    saturn: {
+        short: 'The sixth planet in our solar system',
+        long: {
+            title: "Saturn",
+            paragraph: [
+                "Saturn is the sixth planet from the Sun and the second largest planet in our solar system. Adorned with a dazzling system of icy rings, Saturn is unique among the planets.",
+                "It is not the only planet to have rings, but none are as spectacular or as complex as Saturn's. Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium. The farthest planet from Earth discovered by the unaided human eye, Saturn has been known since ancient times. The planet is named for the Roman god of agriculture and wealth, who was also the father of Jupiter."
+            ]
+        },
+    },
+    uranus: {
+        short: 'The seventh planet in our solar system',
+        long: {
+            title: "Uranus",
+            paragraph: [
+                "Uranus is a very cold and windy planet. It is surrounded by faint rings, and more than two dozen small moons as it rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin on its side.",
+                "Uranus is blue-green in color due to large amounts of methane, which absorbs red light but allows blues to be reflected back into space. The atmosphere is mostly hydrogen and helium, but also includes large amounts of water, ammonia and methane."
+            ]
+        },
+    },
+    neptune: {
+        short: 'The first planet discovered with math',
+        long: {
+            title: "Big Blue",
+            paragraph: [
+                "Dark, cold and whipped by supersonic winds, giant Neptune is the eighth and most distant major planet orbiting our Sun. More than 30 times as far from the Sun as Earth, Neptune is not visible to the naked eye. In 2011, Neptune completed its first 165-year orbit since its discovery.",
+                "The planet’s rich blue color comes from methane in its atmosphere, which absorbs red wavelengths of light, but allows blue ones to be reflected back into space."
+            ]
+        },
+    },
+};
+
+export default descriptions;
